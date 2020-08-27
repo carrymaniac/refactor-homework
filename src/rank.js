b080e374b04369fc5bb8af80303c9634e2954f37
carrymaniac <838537276@qq.com>
2020-08-27 21:15:13 +0800

    [henry] refactor: modify the if statement in method calculateProfitWhenHistoryHaveChina

--- src/rank.js
+++ src/rank.js
@@ -35,11 +35,8 @@ const calculateProfitWhenHistoryHaveChina = (result, history, voyage) => {
     if (history.length > 10) {
         result += 1;
     }
-    if (voyage.length > 12) {
+    if (voyage.length > 12 && voyage.length <= 18) {
         result += 1;
-    }
-    if (voyage.length > 18) {
-        result -= 1;
     }
     return result;
 }
